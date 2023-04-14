f69263c7fcc09519c9a44d50c0bf8cad514ece91
RealTong <i@wst.sh>
2023-04-14 22:55:49 +0800

    feat: 🎸 Init
    Init

---
+++ src/main/java/sh/wst/springsecurifyfido2/SpringSecurifyFido2Application.java
@@ -0,0 +1,13 @@
+package sh.wst.springsecurifyfido2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurifyFido2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurifyFido2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sh/wst/springsecurifyfido2/SpringSecurifyFido2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.wst.springsecurifyfido2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurifyFido2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
